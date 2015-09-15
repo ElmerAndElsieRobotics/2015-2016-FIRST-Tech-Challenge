b7989cf732c6f103f1d7f0d0a6179bbfa68f6696
Matt Chilek <mchilek@gmail.com>
2015-09-15 13:20:31 -0500

    Demo change

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/IrSeekerOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/IrSeekerOp.java
@@ -44,7 +44,7 @@ import com.qualcomm.robotcore.hardware.IrSeekerSensor;
  * Make sure the side of the beacon with the LED on is facing the robot. <br>
  * Turn on the IR beacon. The robot will now follow the IR beacon. <br>
  * To stop the robot, turn the IR beacon off. <br>
- * Demonstrate change by user mchilek
+ * Demonstrate change by user mchilek 123
  */
 public class IrSeekerOp extends OpMode {
 
